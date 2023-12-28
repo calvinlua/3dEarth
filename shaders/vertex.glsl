aea9e75e4fd25d72269da1c94802d7870c62a7cd
Calvin Lua <wenzhenglua@gmail.com>
2023-12-28 17:19:12 +0800

    Added fragment shader formula for atmosphere and enviroment
    Added the formula for fragment shaders for environment and atmosphere

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -1,10 +1,11 @@
 varying vec2 vertexUV;//pass x and y coordinates to fragment.sglsl
-
+varying vec3 vertexNormal; // where the direction of the vertex is facing
 
 void main(){
 //  glsl language , type 
 // vertex shader to display correctly 
 vertexUV = uv; // attribute vec2 uv; provided by threejs by default
+vertexNormal= normal;// attribute vec2 normal; provided by threejs by default
 gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );  //x,y,z,translate/transform w
  
 }
