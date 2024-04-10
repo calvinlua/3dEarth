201e10567977dc193f55e2505868f6c0dfe31e5e
Calvin Lua <wenzhenglua@gmail.com>
2024-04-10 19:26:56 +0800

    Refactor boxAnimation

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ import atmosphereFragmentShader from "./shaders/atmosphereFragment.glsl";
 import gsap from "gsap";
 import { fetchVisitorData } from "./controllers/posthog";
 import { performLineAnimations } from "./utils/lineAnimation";
+import { createBox } from "./utils/boxAnimation";
 import ThreeGlobe from "three-globe";
 
 import { TrackballControls } from "three/addons/controls/TrackballControls.js";
@@ -211,54 +212,6 @@ setInterval(() => {
 //   endLongitude: 103.8198,
 // });
 // group.add(tubeGeometry1);
-
-// putting {} inside the param function will not need to care about the order you put into the data
-function createBox({ lat, long, country, population }) {
-  //can use raycaster in three.js to show some value on the sphere point
-  const box = new THREE.Mesh(
-    new THREE.BoxGeometry(0.1, 0.1, 0.4), //scale x , scale y , scale z
-    new THREE.MeshBasicMaterial({
-      color: "#3BF7FF",
-      opacity: 0.4, // need to make transparent property true only work
-      transparent: true,
-    })
-  );
-
-  //latitude +ve °N upper half , -ve °S lower half
-  //longitude +ve °E right half , -ve °W left half of globe
-  //convert degree angle to radian
-  const latitude = (lat / 180) * Math.PI;
-  const longitude = (long / 180) * Math.PI;
-
-  // formula for the 3D space on longitude and latitude, the angle here is radian (latitude and longitude)
-  const x = earthRadius * Math.cos(latitude) * Math.sin(longitude);
-  const y = earthRadius * Math.sin(latitude);
-  const z = earthRadius * Math.cos(latitude) * Math.cos(longitude);
-
-  box.position.x = x;
-  box.position.y = y;
-  box.position.z = z;
-
-  box.lookAt(0, 0, 0); //perpendicular , boxgeometry always look at the center because sphere is default at 0,0,0
-  box.geometry.applyMatrix4(
-    new THREE.Matrix4().makeTranslation(0, 0, -0.2) //translate object in 3d SPACE x,y,z , 0.8/2
-  ); //translate those boxs that are hiding inside the globe
-
-  group.add(box);
-
-  gsap.to(box.scale, {
-    z: 1.4,
-    duration: 2, // 5 sec animation
-    yoyo: true,
-    repeat: -1, //repeat infinite times
-    ease: "linear",
-    delay: Math.random(),
-  });
-
-  box.country = country; //define country property
-  box.population = population; //define population of visitor
-  // box.scale.z = 0; // scaling the box in z direction
-}
 
 createBox({
   lat: 1.3521,
@@ -542,3 +495,5 @@ addEventListener("touchend", (event) => {
   window.blockMenuHeaderScroll = false;
   console.log("touchend scroll:" + window.blockMenuHeaderScroll);
 });
+
+export { raycaster, earthRadius, group };
